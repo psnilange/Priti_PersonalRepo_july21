1c9a0c6a3809688fe7c06494ce0392775bca0ec1
Priti Vattanavar <psvattanavar@gmail.com>
2021-08-31 17:33:58 +0530

    sencond commit

--- src/package1/class1.java
+++ src/package1/class1.java
@@ -3,6 +3,6 @@ package package1;
 public class class1 {
 	public static void main(String[] a) {
 		System.out.println("Hello world 123");
-		System.out.println("Hello world");
+		System.out.println("Hello world ");
 	}
 }
