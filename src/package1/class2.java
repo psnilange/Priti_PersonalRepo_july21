0e61eafecf78a089badc0616569dc2ae698e61b9
Priti Vattanavar <psvattanavar@gmail.com>
2021-08-31 17:42:13 +0530

    please ignore this commit

--- src/package1/class2.java
+++ src/package1/class2.java
@@ -2,8 +2,7 @@ package package1;
 
 public class class2 {
 	public static void main(String[] a) {
-		System.out.println("Hello world 456");
-		System.out.println("Hello world 456");
-		System.out.println("Hello world 789");
+		
+		
 	}
 }
